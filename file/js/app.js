011361783c17f7bd49cc4983721945c268829c75
saeedzarei23 <100031796+saeedzarei23@users.noreply.github.com>
2022-08-10 20:24:15 +0430

    Add files via upload

--- file/js/app.js
+++ file/js/app.js
@@ -7,7 +7,8 @@ const form = document.querySelector("#clac"),
   budgetText = document.querySelector("#budget-text"),
   //   this the div that we are going to add expece to it
   hazine = document.querySelector(".hazine"),
-  income= document.querySelector('.income-detail')
+  section = document.querySelector("#section"),
+  income = document.querySelector(".income-detail");
 
 // this varibel is for number of buget
 let userBudget, budget;
@@ -40,18 +41,17 @@ class pageElemant {
     const div = document.createElement("div");
     div.classList.add("expence-box");
     div.innerHTML = `<span class="name-expance">${name}</span>
-    <span class="amount-expance">${amount}</span>
-    <span id="remove" class="amount-expance">X</span>`;
+    <span id="ex-num" class="amount-expance">${amount}</span>
+    <button id="remove-expance" class="amount-expance">X</button>`;
     hazine.appendChild(div);
   }
-  addIncome(name,amount){
+  addIncome(name, amount) {
     const div = document.createElement("div");
     div.classList.add("expence-box");
     div.innerHTML = `<span class="name-expance">${name}</span>
-    <span class="amount-expance">${amount}</span>
-    <span id="remove" class="amount-expance">X</span>`
-    income.appendChild(div)
-    
+    <span id="in-num" class="amount-income">${amount}</span>
+    <button id="remove-income" class="amount-expance">X</button>`;
+    income.appendChild(div);
   }
   trackbudget(amount) {
     const moneyadd = budget.budgetPlus(amount);
@@ -71,36 +71,45 @@ form.addEventListener("submit", (e) => {
   const amount = document.querySelector("#amount").value;
   // this is  for our diffrent option that we can chose if we want to add expance to list or buget
   const reduceOrPlus = document.querySelector("#option").value;
-  if(amount===""||name==="")return
-  
-  if (reduceOrPlus === "-") {
+  if (amount === "" || name === "") return;
+
+  if (reduceOrPlus === "minus") {
     page.addExpenstolist(name, amount);
-    page.addmore(amount)
-   
+    page.addmore(amount);
   } else {
-    page.addIncome(name,amount)
+    page.addIncome(name, amount);
     page.addBudget(amount);
-    page.trackbudget(amount)
-   
+    page.trackbudget(amount);
   }
-  if(budget.budget===0  ||budget.budget<0){
-    alert('پولت تمام شده بلند شو برو کار کن')
-    return
+  if (budget.budget === 0 || budget.budget < 0) {
+    alert("پولت تمام شده بلند شو برو کار کن");
+    return;
   }
+});
+document.addEventListener("click", (e) => {
+  const removeIncome = document.querySelector("#remove-income");
+  const removeExpence = document.querySelector("#remove-expance");
+  const amountExpence = Number(document.querySelector("#ex-num").innerText);
+  const amountIncome = Number(document.querySelector("#in-num").innerText);
+  if (e.target === removeExpence) {
+    e.target.parentElement.remove();
+    page.trackbudget(amountIncome);
+  }
+  if (e.target === removeIncome) {
+    e.target.parentElement.remove();
+    page.addmore(amountExpence);
+  }
+  console.log(e.target); 
 });
 document.addEventListener("DOMContentLoaded", function () {
   userBudget = prompt("بودجه شما چقدر است");
-  if (
-    userBudget === null ||
-    userBudget === "" ||
-    userBudget === undefined ||
-    userBudget === 0
-    ) {
-      window.location.reload();
-    } else {
-      budget = new BudgetCalc(userBudget);
-      
-      page.addBudget(budget.budget);
-    }
-  });
-  const page = new pageElemant();
+  if (userBudget === null || userBudget === "" || userBudget === 0) {
+    window.location.reload();
+  } else {
+    budget = new BudgetCalc(userBudget);
+
+    page.addBudget(budget.budget);
+  }
+});
+
+const page = new pageElemant();
